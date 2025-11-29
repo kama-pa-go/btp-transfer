9cee18a6c4328d59c174eb351fac504d61f794c9
Kamila Paulina Godlewska <kamapago@gmail.com>
2025-11-29 19:33:23 +0100

    Insert first wallet (according to task content)

--- init.sql
+++ init.sql
@@ -7,3 +7,9 @@ CREATE TABLE IF NOT EXISTS wallets (
     -- If preferable INT32 may be changed for BIGINT
     balance INT32 NOT NULL CHECK (balance >= 0)
     );
+
+-- Add first wallet with 1,000,000 tokens
+-- ON CONFLICT DO NOTHING -if this wallet already exist dodge conflict
+INSERT INTO wallets (address, balance)
+VALUES ('0x0000000000000000000000000000000000000000', 1000000)
+    ON CONFLICT (address) DO NOTHING;
